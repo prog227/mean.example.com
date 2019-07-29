c9abd3fc14100ca592d67f175e3d7278af828bb3
Marsha <marshareed11860@gmail.com>
2019-07-29 15:31:02 -0500

    Add a registration form

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   console.log(req.session);
   res.render('index', { title: 'Express' });
-});
+});*/
 
 router.get('/logout', function(req, res){
   console.log(req.session);
