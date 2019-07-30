c2d5e44c7ef694031badeb1016e2cec8d16c8ac8
Marsha <marshareed11860@gmail.com>
2019-07-30 11:31:41 -0500

    Show/Hide links based on session data

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var express = require('express');
+var router = express.Router();
+
+router.get('/app', function(req, res, next) {
+  res.render('users/app', { title: 'User Management' });
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
 
 module.exports = router;
