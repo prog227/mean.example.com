9ea92beda3e23a9079423a8371b0dcb67a7cd9ea
Marsha <marshareed11860@gmail.com>
2019-10-25 14:01:48 -0500

    Retrieve a single article

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -2,6 +2,12 @@ var express = require('express');
 var Articles = require('../../models/articles');
 var router = express.Router();
 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  console.log(req.session);
+  res.render('articles', { title: 'Articles'});
+});
+
 router.get('/', function(req, res, next) {
 
   Articles.find({},function(err, articles){
@@ -12,5 +18,15 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/:articleId', function(req,res){
+  
+  var articleId = req.params.articleId;
+   Articles.findOne({'_id':articleId}, function(err, articles){
+     if(err){
+      return res.json({'success':false, 'error': err});
+    }
+     return res.json({'success':true, 'article': articles});
+   });
+ });
 
 module.exports = router;
