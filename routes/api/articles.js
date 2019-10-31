555ba5ef27928bd973dd68e854edbc1030952ee8
Marsha <marshareed11860@gmail.com>
2019-10-30 19:54:24 -0500

    Articles api/articles and /articles pages serving

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -1,16 +1,16 @@
 var express = require('express');
-var Articles = require('../../models/articles');
+var articles = require('../../models/articles');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log(req.session);
-  res.render('articles', { title: 'Articles'});
-});
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   console.log(req.session);
+//   res.render('articles', { title: 'Articles'});
+// });
 
 router.get('/', function(req, res, next) {
 
-  Articles.find({},function(err, articles){
+  articles.find({},function(err, articles){
     if(err){
     return res.json({'success':false, 'error': err});
  }
@@ -18,15 +18,96 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.get('/:articleId', function(req,res){
+router.get('/:id', function(req,res){
   
-  var articleId = req.params.articleId;
-   Articles.findOne({'_id':articleId}, function(err, articles){
+  var id = req.params.id;
+   articles.findOne({'_id':id}, function(err, article){
      if(err){
       return res.json({'success':false, 'error': err});
     }
      return res.json({'success':true, 'article': articles});
    });
  });
+
+router.post('/', function(req, res) {
+  articles.create(new articles({
+    title: req.body.title,
+    description: req.body.description,
+    keywords: req.body.keywords,
+    body: req.body.body,
+    published: req.body.published
+  }), function(err, article){
+    
+    if(err){
+      return res.json({success: false, article: req.body, error: err});
+    }
+
+    return res.json({success: true, article: article});
+    
+  });
+});
+
+ router.put('/', function(req, res){
+
+   articles.findOne({'_id': req.body._id}, function(err, article){
+
+    if(err) {
+      return res.json({success: false, error: err});
+    }
+
+    if(article) {
+
+     let data = req.body;
+
+     if(data.title){
+       article.title = data.title;
+     };
+
+     if(data.description){
+     article.description = data.description;
+     };
+
+     if(data.keywords){
+     article.keywords = data.keywords;
+     };
+
+     if(data.body){
+     article.body = data.body;
+     };
+     
+     if(data.published){
+       article.published = data.published;
+       article.offset = new Date(data.published).getTimezoneOffset();
+     }
+
+     article.save(function(err){
+       if(err){
+         return res.json({success: false, error: err});
+       }else{
+         return res.json({success: true, article:article});
+       }
+     });
+
+    }
+
+   });
+  
+ });
+
+ router.delete('/:articleId', function(req,res){
+
+  var articleId = req.params.articleId;
+
+  articles.remove({'_id':articleId}, function(err,removed){
+
+    if(err){
+      return res.json({success: false, error: err});
+    }
+
+    return res.json({success: true, status: removed});
+
+  });
+
+});
 
 module.exports = router;
--- routes/articles.js
+++ routes/articles.js
@@ -1,17 +1,33 @@
 var express = require('express');
 var router = express.Router();
 var Articles = require('../models/articles');
+var Today = new Date();
 
 router.get('/', function(req, res, next) {
-  res.render('articles/index', { title: 'Articles home' });
+  Articles.find({},function(err, articles){
+    console.log(articles);
+    if(err)
+    {
+      return handleError(err);
+    }
+      return res.render('articles/index', { title: 'Articles' , articles:articles});
+  });
 });
 
-router.get('/:slug', function(req, res, next) {
-  res.render('articles/view', { title: 'Articles' });
+router.get('/view/:slug', function(req, res, next) {
+  Articles.findOne({slug:req.params.slug},function(err, articles){
+    if(err)
+    {
+      return handleError(err);
+    }
+    else{
+      return res.render('articles/view', { title: 'Articles' , article:articles});
+    }
+  });
 });
 
-router.get('/cms', function(req, res, next) {
-  res.render('articles/cms', { title: 'CMS' });
+router.get('/app', function(req, res, next) {
+  res.render('articles/app', { title: 'Article Management' });
 });
 
 module.exports = router;
